ed688f46e45877b6fab8fe8a9ec55a002b72f1fe
Dean Oakley <dean@thriveweb.com.au>
2024-07-23 09:46:48 +1000

    Tidy pinzolo.js

--- js/pinzolo.js
+++ js/pinzolo.js
@@ -1,50 +1,46 @@
 jQuery(document).ready(function($) {
-								
-	
-	jQuery('<span class="sf-sub-indicator"></span>').appendTo('.menu-item-has-children > a');
-	
-	//clear search on click
-	defaultValue = jQuery('#Searchform').val();  
-	jQuery('#Searchform').click(function() {
-		if( this.value == defaultValue ) {
-			jQuery(this).val("");
-		}
-	});
-	
-	//tinyNav
-    jQuery('#menuUl').tinyNav();
-    
-	jQuery('html').addClass('js');
-	
+    // Add dropdown indicator to menu items with children
+    $('.menu-item-has-children > a').append('<span class="sf-sub-indicator"></span>');
 
-	jQuery(window).scroll(function() {
-	    var scroll = jQuery(window).scrollTop();
-
-	    if (scroll > 0) {
-	    	jQuery('#navwrap').addClass('scrolled');
-	    } else {
-	    	jQuery('#navwrap').removeClass('scrolled');
-	    }
-	});
-
-	jQuery(".toggalnav").click(function(){
-        jQuery("body").toggleClass("show_menu");
+    // Clear search input on click
+    var defaultSearchValue = $('#Searchform').val();
+    $('#Searchform').click(function() {
+        if (this.value === defaultSearchValue) {
+            $(this).val('');
+        }
     });
 
-	if(jQuery(window).width() < 600){
+    // Initialize TinyNav for responsive menu
+    $('#menuUl').tinyNav();
 
-		jQuery('.sf-sub-indicator').hide();
-		jQuery('<span class="sf-sub-indicator_mobile"></span>').insertAfter("nav li.menu-item-has-children > a");
-	    jQuery(".sf-sub-indicator_mobile").click(function(e){
-	        e.preventDefault();
-	        jQuery(this).parents('.menu-item-has-children').find('.sub-menu').toggleClass("open");
-	        jQuery(this).toggleClass("btn-open");
-	    });
-	}else{
+    // Add 'js' class to HTML element
+    $('html').addClass('js');
 
-		jQuery('nav ul').superfish(); 
+    // Add 'scrolled' class to navigation when scrolling
+    $(window).scroll(function() {
+        $('#navwrap').toggleClass('scrolled', $(window).scrollTop() > 0);
+    });
 
-	}
-    
+    // Toggle menu on mobile
+    $('.toggalnav').click(function() {
+        $('body').toggleClass('show_menu');
+    });
 
+    // Mobile-specific menu interactions
+    if ($(window).width() < 600) {
+        $('.sf-sub-indicator').hide();
+        $('nav li.menu-item-has-children > a').after('<span class="sf-sub-indicator_mobile"></span>');
+        
+        $('.sf-sub-indicator_mobile').click(function(e) {
+            e.preventDefault();
+            $(this)
+                .parents('.menu-item-has-children')
+                .find('.sub-menu')
+                .toggleClass('open');
+            $(this).toggleClass('btn-open');
+        });
+    } else {
+        // Initialize Superfish menu for desktop
+        $('nav ul').superfish();
+    }
 });
