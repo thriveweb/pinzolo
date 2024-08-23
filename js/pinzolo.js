408a3df6650be4c9e8bf1cde89a1d1ebc131bcca
Dean Oakley <dean@thriveweb.com.au>
2024-08-23 15:08:49 +1000

    Tidy pinzolo.js

--- js/pinzolo.js
+++ js/pinzolo.js
@@ -1,48 +1,40 @@
 jQuery(document).ready(function($) {
-								
-	
-	jQuery('<span class="sf-sub-indicator"></span>').appendTo('.menu-item-has-children > a');
-	
-	//clear search on click
-	defaultValue = jQuery('#Searchform').val();  
-	jQuery('#Searchform').click(function() {
-		if( this.value == defaultValue ) {
-			jQuery(this).val("");
-		}
-	});
-	
-    
-	jQuery('html').addClass('js');
-	
 
-	jQuery(window).scroll(function() {
-	    var scroll = jQuery(window).scrollTop();
+    // Add sub-indicator to menu items with children
+    $('<span class="sf-sub-indicator"></span>').appendTo('.menu-item-has-children > a');
 
-	    if (scroll > 0) {
-	    	jQuery('#navwrap').addClass('scrolled');
-	    } else {
-	    	jQuery('#navwrap').removeClass('scrolled');
-	    }
-	});
-
-	jQuery(".toggalnav").click(function(){
-        jQuery("body").toggleClass("show_menu");
+    // Clear search field on click if it contains the default value
+    var defaultValue = $('#Searchform').val();
+    $('#Searchform').click(function() {
+        if (this.value === defaultValue) {
+            $(this).val("");
+        }
     });
 
-	if(jQuery(window).width() < 600){
+    // Add 'js' class to HTML tag
+    $('html').addClass('js');
 
-		jQuery('.sf-sub-indicator').hide();
-		jQuery('<span class="sf-sub-indicator_mobile"></span>').insertAfter("nav li.menu-item-has-children > a");
-	    jQuery(".sf-sub-indicator_mobile").click(function(e){
-	        e.preventDefault();
-	        jQuery(this).parents('.menu-item-has-children').find('.sub-menu').toggleClass("open");
-	        jQuery(this).toggleClass("btn-open");
-	    });
-	}else{
+    // Toggle 'scrolled' class based on scroll position
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        $('#navwrap').toggleClass('scrolled', scroll > 0);
+    });
 
-		//jQuery('nav ul').superfish(); 
+    // Toggle menu visibility on click
+    $(".toggalnav").click(function() {
+        $("body").toggleClass("show_menu");
+    });
 
-	}
-    
+    if ($(window).width() < 600) {
+        // Hide default sub-indicator and add mobile-specific indicator
+        $('.sf-sub-indicator').hide();
+        $('<span class="sf-sub-indicator_mobile"></span>').insertAfter("nav li.menu-item-has-children > a");
 
+        // Toggle sub-menu visibility on mobile
+        $(".sf-sub-indicator_mobile").click(function(e) {
+            e.preventDefault();
+            $(this).parents('.menu-item-has-children').find('.sub-menu').toggleClass("open");
+            $(this).toggleClass("btn-open");
+        });
+    }
 });
